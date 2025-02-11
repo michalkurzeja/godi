6af30bb26c1052682c998f5c4763ccfa2e3fd994
Michał Kurzeja <mike.kurzeja@gmail.com>
2025-02-11 17:50:33 +0100

    fix: lint

--- di_test.go
+++ di_test.go
@@ -1155,7 +1155,6 @@ func TestGodi_Services(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -36,7 +36,6 @@ func TestFuncName(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
